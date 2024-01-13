e4b4c1f0054aed71f9577a09981dd11d86a2bf65
Shubham Singh <shbh29@gmail.com>
2024-01-13 13:03:42 +0530

    Builder Pattern Demo

---
+++ src/main/java/org/example/designpatterns/builder/HDD.java
@@ -0,0 +1,8 @@
+package org.example.designpatterns.builder;
+
+public enum HDD {
+    TB_1,
+    TB_2,
+    TB_4,
+    DEFAULT_500GB
+}
---
+++ src/main/java/org/example/designpatterns/builder/RAM.java
@@ -0,0 +1,8 @@
+package org.example.designpatterns.builder;
+
+public enum RAM {
+    DOCUMENTS,
+    SMALL_GAMES,
+    GRAPHICS_GAMES,
+    VIDEO_RENDERING_GAMES
+}
